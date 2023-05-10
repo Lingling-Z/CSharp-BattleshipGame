910039dbe13dab6f258749039bb39e81cc65c753
Lingling-Z <97594629+Lingling-Z@users.noreply.github.com>
2023-05-10 12:12:19 +1200

    initial commit

---
+++ BattleshipApp/Program.cs
@@ -0,0 +1,31 @@
+﻿using BattleshipClassLibrary;
+using BattleshipClassLibrary.Models;
+
+WelcomeMessage();
+PlayerInfoModel player1 = CreatePlayer();
+//ask for ship placement
+//validate spot
+//store
+//clear
+
+
+
+static void WelcomeMessage()
+{
+    Console.WriteLine("Welcome to the Battleship game!");
+    Console.WriteLine();
+}
+static string GetUsersName()
+{
+    Console.Write("What is your name: ");
+    string output = Console.ReadLine();
+    return output;
+}
+static PlayerInfoModel CreatePlayer()
+{
+    PlayerInfoModel output = new PlayerInfoModel();
+    output.UserName = GetUsersName();
+    GameLogic.InitializeGrid(output);
+
+    return output;
+}
---
+++ BattleshipClassLibrary/GameLogic.cs
@@ -0,0 +1,34 @@
+﻿using BattleshipClassLibrary.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BattleshipClassLibrary
+{
+    public static class GameLogic
+    {
+        public static void InitializeGrid(PlayerInfoModel player)
+        {
+            List<string> letters = new List<string> { "A", "B", "C", "D", "E" };
+            List<int> numbers = new List<int> { 1, 2, 3, 4, 5 };
+            foreach(string letter in letters)
+            {
+                foreach(int number in numbers)
+                {
+                    AddGridSpot(player, letter, number);
+                }
+            }
+        }
+        private static void AddGridSpot(PlayerInfoModel player, string letter, int number)
+        {
+            GridSpotModel spot = new GridSpotModel
+            {
+                SpotLetter = letter,
+                SpotNumber = number
+            };
+            player.ShotGrid.Add(spot);
+        }
+    }
+}
